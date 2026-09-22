be850b7ad1ebf4de375c2fb8b3e017927de5d691
ismael2031 <ismaelgorosito123@gmail.com>
2026-09-21 22:26:00 -0300

    Habilita boton de Pull Request

--- SimuladorEcosistema/src/simuladorecosistema/Ecosistema.java
+++ SimuladorEcosistema/src/simuladorecosistema/Ecosistema.java
@@ -2,6 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+
+//Creado por el isma
 package simuladorecosistema;
 import java.util.ArrayList;
 
