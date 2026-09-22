b03a6ed56b9d0b86ec219534454e829d93608283
ismael2031 <ismaelgorosito123@gmail.com>
2026-09-21 22:11:24 -0300

    Agrega detalles y conecta Ecosistema

--- SimuladorEcosistema/src/simuladorecosistema/Ecosistema.java
+++ SimuladorEcosistema/src/simuladorecosistema/Ecosistema.java
@@ -2,6 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+// Creado por Ismael
 package simuladorecosistema;
 import java.util.ArrayList;
 
